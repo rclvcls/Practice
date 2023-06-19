a74eb7bbf448680c2b5684ddcb537b36159f9154
ar <52906300+rclvcls@users.noreply.github.com>
2023-06-19 11:02:45 +0800

    Set up

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav class="bg-gray-800">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+   <RouterView />
+</template>
+
+<style scoped>
+</style>
